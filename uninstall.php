94df0cd930f878ef89b426ab90593a56dbf8aedd
salubrio <salubrio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-23 16:40:42 +0000

    bruce force delete_options
    git-svn-id: https://plugins.svn.wordpress.org/add-code-to-head/trunk@602709 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -8,6 +8,24 @@ if( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	<p>Don't call this file directly. If you wish to uninstall, use the admin tool to do it.</p>";
 	wp_die( $message );
 } else {
-	delete_option( 'acth_options' );
+	$the_option = "acth_options";
+	
+	/*
+	  If running in multisite environment,
+	  we'll need to remove the option by brute force
+	*/
+	if( is_multisite() ) {
+    	global $wpdb;
+    	$blogs = $wpdb->get_results("SELECT blog_id FROM {$wpdb->blogs}", ARRAY_A);
+    	if( $blogs ) {
+        	foreach( $blogs as $blog ) {
+            	switch_to_blog( $blog['blog_id'] );
+    	        delete_option( $the_option );
+        	}
+        	restore_current_blog();
+   		 }
+	} else {
+		delete_option( $the_option );
+	}
 }
 ?>
